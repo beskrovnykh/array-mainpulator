bc9bd9de4fe7e1684b18bebad6fc76f938286b56
Бескровных Андрей Владимирович <beskrovnykh@proitr.ru>
2022-10-06 22:35:24 +0800

    Test parameterized

--- ParallelArrayManipulatorTest.java
+++ ParallelArrayManipulatorTest.java
@@ -1,60 +1,53 @@
 import org.junit.Test;
+import java.util.stream.*;
+import java.util.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.experimental.runners.Enclosed;
+
 import static org.junit.Assert.*;
 
- 
+@RunWith(Enclosed.class)
 public class ParallelArrayManipulatorTest {
- 
-    @Test
-    public void testOne() {
-        int[][] input = new int[3][3];
 
-        input[0] = new int[] {1, 2, 100};
-        input[1] = new int[] {2, 5, 100};
-        input[2] = new int[] {3, 4, 100};
-        
-        assertEquals(200, ParallelArrayManipulator.arrayManipulation(5, input));
+    public static class ArrayManipulatorTest {
+        @Test
+        public void testOne() {
+            int[][] input = new int[3][3];
+
+            input[0] = new int[] {1, 2, 100};
+            input[1] = new int[] {2, 5, 100};
+            input[2] = new int[] {3, 4, 100};
+
+            assertEquals(200, ParallelArrayManipulator.arrayManipulation(5, input));
+        }
     }
 
-    @Test
-    public void testMaxSum_whenRandomSeries() {
-        long[] seriesRandom = new long[] { 122, 60, -2, 135, 73, 11, 148 };
-        assertEquals(547, ParallelArrayManipulator.maxSumParallel(seriesRandom));
-        assertEquals(547, ParallelArrayManipulator.maxSumSequential(seriesRandom));
-    }
+    @RunWith(Parameterized.class)
+    public static class MaxSumParrallelTest {
+        @Parameter
+        public long[] series;
 
-    @Test
-    public void testMaxSum_whenPositiveSeries() {
-        long[] seriesPositive = new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        assertEquals(45, ParallelArrayManipulator.maxSumParallel(seriesPositive));
-        assertEquals(45, ParallelArrayManipulator.maxSumSequential(seriesPositive));
-
-    }
-
-    @Test
-    public void testMaxSum_whenNegativeSeries() {
-        long[] seriesNegative = new long[] { -1, -2, -3, -4, -5, -6, -7, -8, -9 }; 
-        assertEquals(0, ParallelArrayManipulator.maxSumParallel(seriesNegative));
-        assertEquals(0, ParallelArrayManipulator.maxSumSequential(seriesNegative));
-    }
-    
-    @Test
-    public void testMaxSum_whenHighLowSeries() {
-        long[] seriesHighLow = new long[] { 1, 2, 3, 4, -1, -2, -3, -4, -4 }; 
-        assertEquals(10, ParallelArrayManipulator.maxSumParallel(seriesHighLow));
-        assertEquals(10, ParallelArrayManipulator.maxSumSequential(seriesHighLow));
-    }
-
-    @Test
-    public void testMaxSum_whenHighLogSeries() {
-        long[] seriesLowHigh = new long[] { -1, -2, -3, -4, 1, 2, 3 }; 
-        assertEquals(0, ParallelArrayManipulator.maxSumParallel(seriesLowHigh));
-        assertEquals(0, ParallelArrayManipulator.maxSumSequential(seriesLowHigh));
-    }
+        @Parameters(name = "{index}: series - {0}")
+        public static List<long[]> seriesProvider() {
+            return Arrays.asList(
+                new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                new long[] {-1,-2,-3,-4,-5,-6,-7,-8,-9 },
+                new long[] { 1, 2, 3, 4, -1, -2, -3, -4, -4 },
+                new long[] { -1, -2, -3, -4, 1, 2, 3 },
+                new long[] { 0, 0, 3, 10, -3, 0, -5 },
+                LongStream.range(1, 10_000_000).map(i -> (i * 137) % 199 - 99).toArray()
+            );
+        }
 
         @Test
-    public void testMaxSum_whenDeltoidSeries() {
-        long[] seriesDelta = new long[] { 0, 0, 3, 10, -3, 0, -5 }; 
-        assertEquals(13, ParallelArrayManipulator.maxSumParallel(seriesDelta));
-        assertEquals(13, ParallelArrayManipulator.maxSumSequential(seriesDelta));
+        public void testMaxSum() {
+            assertEquals(
+                ParallelArrayManipulator.maxSumParallel(series),
+                ParallelArrayManipulator.maxSumSequential(series)
+            );
+        }
     }
 }
